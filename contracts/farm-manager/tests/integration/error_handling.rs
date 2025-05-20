9ea85c14df9c1fd2a474c5991504e549241cc1f5
Samuel Barbosa <sambarbosaa@gmail.com>
2025-05-20 10:03:20 -0500

    refactor(farm-manager-tests): extract repeated values into constants in error_handling tests
    Refactors the test file `contracts/farm-manager/tests/integration/error_handling.rs`
    by extracting hardcoded string and numeric literals into named constants.
    This improves readability and maintainability of the test code.

--- contracts/farm-manager/tests/integration/error_handling.rs
+++ contracts/farm-manager/tests/integration/error_handling.rs
@@ -8,6 +8,20 @@ use mantra_dex_std::farm_manager::{FarmAction, FarmParams, PositionAction};
 use crate::common::suite::TestingSuite;
 use crate::common::MOCK_CONTRACT_ADDR_1;
 
+const UUSDY_DENOM: &str = "uusdy";
+const FARM_ASSET_AMOUNT: u128 = 8_000u128;
+const START_EPOCH: u64 = 12;
+const PRELIMINARY_END_EPOCH: u64 = 16;
+const UNLOCKING_DURATION: u64 = 86_400;
+
+const INVALID_ID_SPECIAL_CHARS: &str = "invalid!";
+const INVALID_ID_TOO_LONG: &str =
+    "7105920181635468364293788789264771059201816354683642937887892647a";
+const INVALID_ID_NON_ASCII: &str = "感";
+const INVALID_ID_SQL_INJECTION_LIKE: &str = "INSERT INTO my_table (my_string) VALUES (values)";
+const VALID_ID: &str = "7105920181635468364293788789264771059201816354683642937887892647";
+const LP_STAKE_AMOUNT: u128 = 5_000;
+
 #[test]
 fn test_farm_and_position_id_validation() {
     let lp_denom = format!("factory/{MOCK_CONTRACT_ADDR_1}/{LP_SYMBOL}").to_string();
@@ -29,17 +43,17 @@ fn test_farm_and_position_id_validation() {
             FarmAction::Fill {
                 params: FarmParams {
                     lp_denom: lp_denom.clone(),
-                    start_epoch: Some(12),
-                    preliminary_end_epoch: Some(16),
+                    start_epoch: Some(START_EPOCH),
+                    preliminary_end_epoch: Some(PRELIMINARY_END_EPOCH),
                     curve: None,
                     farm_asset: Coin {
-                        denom: "uusdy".to_string(),
-                        amount: Uint128::new(8_000u128),
+                        denom: UUSDY_DENOM.to_string(),
+                        amount: Uint128::new(FARM_ASSET_AMOUNT),
                     },
-                    farm_identifier: Some("invalid!".to_string()),
+                    farm_identifier: Some(INVALID_ID_SPECIAL_CHARS.to_string()),
                 },
             },
-            vec![coin(8_000, "uusdy"), coin(1_000, "uom")],
+            vec![coin(FARM_ASSET_AMOUNT, UUSDY_DENOM), coin(1_000, "uom")],
             |result| {
                 let err = result.unwrap_err().downcast::<ContractError>().unwrap();
                 match err {
@@ -55,20 +69,17 @@ fn test_farm_and_position_id_validation() {
             FarmAction::Fill {
                 params: FarmParams {
                     lp_denom: lp_denom.clone(),
-                    start_epoch: Some(12),
-                    preliminary_end_epoch: Some(16),
+                    start_epoch: Some(START_EPOCH),
+                    preliminary_end_epoch: Some(PRELIMINARY_END_EPOCH),
                     curve: None,
                     farm_asset: Coin {
-                        denom: "uusdy".to_string(),
-                        amount: Uint128::new(8_000u128),
+                        denom: UUSDY_DENOM.to_string(),
+                        amount: Uint128::new(FARM_ASSET_AMOUNT),
                     },
-                    farm_identifier: Some(
-                        "7105920181635468364293788789264771059201816354683642937887892647a"
-                            .to_string(),
-                    ),
+                    farm_identifier: Some(INVALID_ID_TOO_LONG.to_string()),
                 },
             },
-            vec![coin(8_000, "uusdy"), coin(1_000, "uom")],
+            vec![coin(FARM_ASSET_AMOUNT, UUSDY_DENOM), coin(1_000, "uom")],
             |result| {
                 let err = result.unwrap_err().downcast::<ContractError>().unwrap();
                 match err {
@@ -84,17 +95,17 @@ fn test_farm_and_position_id_validation() {
             FarmAction::Fill {
                 params: FarmParams {
                     lp_denom: lp_denom.clone(),
-                    start_epoch: Some(12),
-                    preliminary_end_epoch: Some(16),
+                    start_epoch: Some(START_EPOCH),
+                    preliminary_end_epoch: Some(PRELIMINARY_END_EPOCH),
                     curve: None,
                     farm_asset: Coin {
-                        denom: "uusdy".to_string(),
-                        amount: Uint128::new(8_000u128),
+                        denom: UUSDY_DENOM.to_string(),
+                        amount: Uint128::new(FARM_ASSET_AMOUNT),
                     },
-                    farm_identifier: Some("感".to_string()),
+                    farm_identifier: Some(INVALID_ID_NON_ASCII.to_string()),
                 },
             },
-            vec![coin(8_000, "uusdy"), coin(1_000, "uom")],
+            vec![coin(FARM_ASSET_AMOUNT, UUSDY_DENOM), coin(1_000, "uom")],
             |result| {
                 let err = result.unwrap_err().downcast::<ContractError>().unwrap();
                 match err {
@@ -110,19 +121,17 @@ fn test_farm_and_position_id_validation() {
             FarmAction::Fill {
                 params: FarmParams {
                     lp_denom: lp_denom.clone(),
-                    start_epoch: Some(12),
-                    preliminary_end_epoch: Some(16),
+                    start_epoch: Some(START_EPOCH),
+                    preliminary_end_epoch: Some(PRELIMINARY_END_EPOCH),
                     curve: None,
                     farm_asset: Coin {
-                        denom: "uusdy".to_string(),
-                        amount: Uint128::new(8_000u128),
+                        denom: UUSDY_DENOM.to_string(),
+                        amount: Uint128::new(FARM_ASSET_AMOUNT),
                     },
-                    farm_identifier: Some(
-                        "INSERT INTO my_table (my_string) VALUES (values)".to_string(),
-                    ),
+                    farm_identifier: Some(INVALID_ID_SQL_INJECTION_LIKE.to_string()),
                 },
             },
-            vec![coin(8_000, "uusdy"), coin(1_000, "uom")],
+            vec![coin(FARM_ASSET_AMOUNT, UUSDY_DENOM), coin(1_000, "uom")],
             |result| {
                 let err = result.unwrap_err().downcast::<ContractError>().unwrap();
                 match err {
@@ -138,20 +147,17 @@ fn test_farm_and_position_id_validation() {
             FarmAction::Fill {
                 params: FarmParams {
                     lp_denom: lp_denom.clone(),
-                    start_epoch: Some(12),
-                    preliminary_end_epoch: Some(16),
+                    start_epoch: Some(START_EPOCH),
+                    preliminary_end_epoch: Some(PRELIMINARY_END_EPOCH),
                     curve: None,
                     farm_asset: Coin {
-                        denom: "uusdy".to_string(),
-                        amount: Uint128::new(8_000u128),
+                        denom: UUSDY_DENOM.to_string(),
+                        amount: Uint128::new(FARM_ASSET_AMOUNT),
                     },
-                    farm_identifier: Some(
-                        "7105920181635468364293788789264771059201816354683642937887892647"
-                            .to_string(),
-                    ),
+                    farm_identifier: Some(VALID_ID.to_string()),
                 },
             },
-            vec![coin(8_000, "uusdy"), coin(1_000, "uom")],
+            vec![coin(FARM_ASSET_AMOUNT, UUSDY_DENOM), coin(1_000, "uom")],
             |result| {
                 result.unwrap();
             },
@@ -161,11 +167,11 @@ fn test_farm_and_position_id_validation() {
         .manage_position(
             &creator,
             PositionAction::Create {
-                identifier: Some("invalid!".to_string()),
-                unlocking_duration: 86_400,
+                identifier: Some(INVALID_ID_SPECIAL_CHARS.to_string()),
+                unlocking_duration: UNLOCKING_DURATION,
                 receiver: None,
             },
-            vec![coin(5_000, lp_denom.clone())],
+            vec![coin(LP_STAKE_AMOUNT, lp_denom.clone())],
             |result| {
                 let err = result.unwrap_err().downcast::<ContractError>().unwrap();
                 match err {
@@ -179,13 +185,11 @@ fn test_farm_and_position_id_validation() {
         .manage_position(
             &creator,
             PositionAction::Create {
-                identifier: Some(
-                    "7105920181635468364293788789264771059201816354683642937887892647a".to_string(),
-                ),
-                unlocking_duration: 86_400,
+                identifier: Some(INVALID_ID_TOO_LONG.to_string()),
+                unlocking_duration: UNLOCKING_DURATION,
                 receiver: None,
             },
-            vec![coin(5_000, lp_denom.clone())],
+            vec![coin(LP_STAKE_AMOUNT, lp_denom.clone())],
             |result| {
                 let err = result.unwrap_err().downcast::<ContractError>().unwrap();
                 match err {
@@ -199,11 +203,11 @@ fn test_farm_and_position_id_validation() {
         .manage_position(
             &creator,
             PositionAction::Create {
-                identifier: Some("INSERT INTO my_table (my_string) VALUES (values)".to_string()),
-                unlocking_duration: 86_400,
+                identifier: Some(INVALID_ID_SQL_INJECTION_LIKE.to_string()),
+                unlocking_duration: UNLOCKING_DURATION,
                 receiver: None,
             },
-            vec![coin(5_000, lp_denom.clone())],
+            vec![coin(LP_STAKE_AMOUNT, lp_denom.clone())],
             |result| {
                 let err = result.unwrap_err().downcast::<ContractError>().unwrap();
                 match err {
@@ -217,11 +221,11 @@ fn test_farm_and_position_id_validation() {
         .manage_position(
             &creator,
             PositionAction::Create {
-                identifier: Some("感".to_string()),
-                unlocking_duration: 86_400,
+                identifier: Some(INVALID_ID_NON_ASCII.to_string()),
+                unlocking_duration: UNLOCKING_DURATION,
                 receiver: None,
             },
-            vec![coin(5_000, lp_denom.clone())],
+            vec![coin(LP_STAKE_AMOUNT, lp_denom.clone())],
             |result| {
                 let err = result.unwrap_err().downcast::<ContractError>().unwrap();
                 match err {
